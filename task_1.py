b163e0b1317a52797dbd513723a531993d2794e8
Yuriy-St <yuriystaynov@gmail.com>
2024-10-09 13:23:36 +0300

    task_1

---
+++ task_1.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+
+def get_days_from_today(date):
+    try:
+        date = datetime.strptime(date, "%Y-%m-%d")
+        today = datetime.today()
+        dif = today - date
+        print(dif.days)
+    except Exception as e:
+        print(e)
+
+get_days_from_today("2025-10-09")
